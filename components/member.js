1b8e35b7aa1a6a70256d331e37c032b684107a6f
dogooo <gh-masa@svk.jp>
2022-12-12 13:59:29 +0000

    add text

--- components/member.js
+++ components/member.js
@@ -1,5 +1,5 @@
 import styles from '../styles/member.module.css';
-import member1 from '../public/member1.jpg';
+import member1 from '../public/member1.JPG';
 import member2 from '../public/member2.jpg';
 import member3 from '../public/member3.jpg';
 import member4 from '../public/member4.jpg';
@@ -13,24 +13,24 @@ export default function Member() {
       </div>
       <div className={styles.project}>
         <div>
-          <img src="../member1.jpg" alt="makiの画像" />
-          <h3>maki</h3>
-          <p>自己紹介のテキスト自己紹介のテキスト自己紹介のテキスト</p>
+          <img src="../member1.JPG" alt="makiの画像" />
+          <h3 className={styles.name}>maki</h3>
+          <p>ディレクター<br/>NATORIプロジェクト代表</p>
         </div>
         <div>
           <img src="../member2.jpg" alt="もとの画像" />
-          <h3>もと</h3>
-          <p>自己紹介のテキスト自己紹介のテキスト自己紹介のテキスト</p>
+          <h3 className={styles.name}>もと</h3>
+          <p>名取集落責任者<br/>現地在住、地元住民との調整・まとめ役</p>
         </div>
         <div>
           <img src="../member3.jpg" alt="kondo" />
-          <h3>kondo</h3>
-          <p>自己紹介のテキスト自己紹介のテキスト自己紹介のテキスト</p>
+          <h3 className={styles.name}>kondo</h3>
+          <p>Webクリエイター<br/>Web関連およびスマートコントラクト開発</p>
         </div>
         <div>
           <img src="../member4.jpg" alt="みくの画像" />
-          <h3>実紅</h3>
-          <p>自己紹介のテキスト自己紹介のテキスト自己紹介のテキスト</p>
+          <h3 className={styles.name}>実紅</h3>
+          <p>デザイナー<br/>Web・NFTなどのデザイン全般</p>
         </div>
       </div>
     </section>

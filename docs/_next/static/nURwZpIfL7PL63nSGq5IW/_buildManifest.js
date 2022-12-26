78c116f23236aecd9efe9efd29663738e6d5e002
dogooo <gh-masa@svk.jp>
2022-12-26 00:43:15 +0000

    fix font, add goal text

---
+++ docs/_next/static/nURwZpIfL7PL63nSGq5IW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/29b901274864670e.css","static/chunks/pages/index-f075f1c2653cff15.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

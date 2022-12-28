296b298a8642ddd0ed779e462bc649f9a001d8d7
dogooo <gh-masa@svk.jp>
2022-12-28 01:05:06 +0000

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import Header from '../components/header.js';
 import Footer from '../components/footer.js';
-import Head from 'next/head'
+import Head from 'next/head';
+import Script from 'next/script';
+import * as gtag from '../lib/gtag';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             
+             gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
       <Head>
         <title>クリプト集落 NATORIプロジェクト</title>
       </Head>
